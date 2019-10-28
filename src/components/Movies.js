60b4e36d27840d858df8617afe9921abbb78f4b2
George <georuiz817@gmail.com>
2019-10-27 22:19:27 -0400

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,10 +1,32 @@
-import React from 'react';
-import { movies } from '../data';
+/* 
+  Movies
+    16) should render one <h1 /> first, inside of the <div />
+    17) should render 'Movies Page' inside of the <h1 />
+    18) should render a <div /> for each movie
+    19) should render the right content for each movie
+    */
 
+import React from 'react';
+
+
+import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {movies.map((movie, index) => (
+           <div key={index}>
+            <h2>{movie.title}</h2>
+            <h4>{movie.time} Minutes</h4>
+            <p>Genres:</p>
+            <ul>
+              {movie.genres.map((genre, index) =>
+                (<li key={index}>{genre}</li>)
+              )}
+            </ul>
+            <br />
+          </div>
+        ))}
     </div>
   );
 };
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,26 @@
+/*
+  NavBar
+    20) wraps content in a div with .navbar class
+    navlinks
+      21) renders a Home <NavLink>
+      22) renders a Movies <NavLink>
+      23) renders an Actors <NavLink>
+      24) renders a Directors <NavLink>
+  */
+
+
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
   return (
     <div>
-      {/*{code here}*/}
+     <nav className="navbar">
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/movies">Movies</NavLink>
+        <NavLink to="/actors">Actors</NavLink>
+        <NavLink to="/directors">Directors</NavLink>
+     </nav>
     </div>
   );
 };
